d80b73cc39eda164b6c50a34d0d2f6244c8cd3da
siva-ranjani-r <siva2300rs@gmail.com>
2022-12-09 20:10:30 +0530

    Adding js file.

---
+++ js/voterId.js
@@ -0,0 +1,18 @@
+var age=parseInt(prompt("enter age value"));
+var voterId=parseInt(prompt("enter voterId Number value"));
+if(age>18)
+{
+	if(voterId="true")
+	{
+		document.write("Eligible for vote");
+	}
+	else
+	{
+		document.write("Not Eligible");
+	}
+}
+else
+{
+	document.write("Not eligible for vote");
+}
+
